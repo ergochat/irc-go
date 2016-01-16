58333a61fe06418a1903687748ec1f46a4ff7fed
Daniel Oaks <daniel@danieloaks.net>
2016-01-17 09:58:34 +1000

    Add basic package doc

---
+++ doc.go
@@ -0,0 +1,11 @@
+// written by Daniel Oaks <daniel@danieloaks.net>
+// released under the CC0 Public Domain license
+
+/*
+Package girc helps create and manage an IRC connection.
+
+This package is in extremely early stages, and is more for testing than for
+actual production work at this point in time. I do not recommend using this for
+any production purposes at this point.
+*/
+package girc
