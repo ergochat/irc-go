caed10995cd1bdfdd2fc625dd54443e360af0456
Daniel Oaks <daniel@danieloaks.net>
2016-01-17 12:11:44 +1000

    ircmsg: Alpha this up

--- ircmsg/doc.go
+++ ircmsg/doc.go
@@ -4,8 +4,6 @@
 /*
 Package ircmsg helps parse and create lines for IRC connections.
 
-This package is in extremely early stages, and is more for testing than for
-actual production work at this point in time. I do not recommend using this for
-any production purposes at this point.
+This package is in alpha.
 */
 package ircmsg
